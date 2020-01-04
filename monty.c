d1e26c08cba55c4250d7d55613a75a947f6fc6bd
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-04 14:17:23 +0000

    [ADD]: program is able to print every line of the document

--- monty.c
+++ monty.c
@@ -3,28 +3,33 @@
 /**
  * main - test the code for operation codes
  *
- *
- *
- *
+ * @ac: argument count.
+ * @av: vector of strings
+ * Return: 0 on succes, exit failure on error.
  */
 
 int main(int ac, char **av)
 {
+	FILE *fd;
+	char buffer[100];
 
-
-	int fd = open(av[1]);
-
+	fd = fopen(av[1], "r");
 	if (ac != 2)
 	{
-		dprintf(STDERR_FILENO, "USAGE: monty file");
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	if (fd == -1)
+	if (fd == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't open file %s", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
 
-	fclose(fd)
+	while (fgets(buffer, 100, fd)!= NULL)
+	{
+		puts(buffer);
+	}
+
+	fclose(fd);
 	return (0);
 }
