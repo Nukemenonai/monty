561b3767edd4310bff4e3357f84c68b0d53e8365
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-04 13:46:35 +0000

    [feature] added libraries for runtime use

--- monty.h
+++ monty.h
@@ -1,6 +1,15 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
